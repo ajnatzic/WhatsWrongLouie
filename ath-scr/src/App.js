bb697a85e24d9f1d2dd959d64a1099cae469886a
AJ Natzic <alfred.natzic@sbcglobal.net>
2020-10-15 16:31:33 -0400

    new outline for React front-end

---
+++ ath-scr/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import 'react-bootstrap';
+// import logo from './logo.svg';
+import './App.css';
+
+// Bootstrap imports
+import div from '../node_modules/react-bootstrap'
+
+// JSX files
+import Navbar from './navbar';
+
+
+
+function App() {
+  return (
+
+  );
+}
+
+export default App;
