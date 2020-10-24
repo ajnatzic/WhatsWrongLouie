610d886af743e4cfe60c819a314a8b3ead0a998c
AJ Natzic <42810761+ajnatzic@users.noreply.github.com>
2020-10-24 18:08:28 -0400

    Create template for Athlete front-end

--- ath-scr/src/athleteScreen.jsx
+++ ath-scr/src/athleteScreen.jsx
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
-// Athlete Creation Form
-import AthleteCreateForm from './athleteCreateForm';
+// Import Athlete Creation Screen (includes form and model of "athlete")
+import AthleteCreateScreen from './athleteCreateForm';
+
+/** Home Screen */
+import AthleteHomeScreen from './athleteHomeScreen';
+
+/** Treat Screen */
+import AthleteTreatScreen from './athleteTreatScreen';
+
+/** HOPRS screens */
+import AthleteHistory from './hoprs/athleteHistory';
+import AthleteObservation from './hoprs/athleteObservation';
+import AthletePalpation from './hoprs/athletePalpation';
+import AthleteROM from './hoprs/athleteROM';
+import AthleteSpecialTests from './hoprs/athleteSpecialTest';
+
+/** Error screen (if a screen cannot load for some reason) */
+import Error from './errorPage';
 
 
 class AthleteScreen extends Component {
@@ -14,7 +30,8 @@ class AthleteScreen extends Component {
 
     // Upon clicking "Create new Athlete" Button
     handleNewAthlete() {
-        this.setState({ mode: 'newAthlete' });
+        console.log('button clicked!!!!');
+        this.setState({ mode: 'athNew' });
     }
 
 
@@ -23,19 +40,55 @@ class AthleteScreen extends Component {
         const renderStatus = () => {
             let com = '';
             switch (this.state.mode) {
-                case 'newAthlete':
-                    com = <AthleteCreateForm />
+                /** Begin Welcome Screens. New users start here */
+                case 'snot':
+                    com = <button id="createNewAthlete" type="button" className="btn btn-primary btn-lg" onClick={this.handleNewAthlete}>Create New Athlete</button>
+                    break;
+                // Athlete Create Screen
+                case 'athNew':
+                    com = <AthleteCreateScreen />
+                    break;
+
+                
+                /** Begin main game screens. Returning users start here */
+                // Athlete Home Screen
+                case 'athHome':
+                    com = <AthleteHomeScreen />
+                    break;
+                // Treat screen
+                case 'athTreat':
+                    com = <AthleteTreatScreen />
+                    break;
+
+                /** Begin HOPRS screens */
+                // History screen
+                case 'athHistory':
+                    com = <AthleteHistory />
+                    break;
+                // Observation screen
+                case 'athObservation':
+                    com = <AthleteObservation />
+                    break;
+                // Palpation screen
+                case 'athPalpation':
+                    com = <AthletePalpation />
+                    break;
+                // Range of Motion screen
+                case 'athROM':
+                    com = <AthleteROM />
+                    break;
+                // Special Tests screen
+                case 'athST':
+                    com = <AthleteSpecialTests />
                     break;
                 default:
-                    // com = <Error />
+                    com = <Error />
                     break;
             }
             return com;
         }
         return (
-
             <div className="container">
-                <button id="createNewAthlete" type="button" className="btn btn-primary btn-lg" onClick={this.handleNewAthlete}>Create New Athlete</button>
                 <div className="mainscreen">
                     {renderStatus()}
                 </div>
---
+++ ath-scr/src/hoprs/athletePalpation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AthletePalpation extends Component {
+    render() {
+        return (
+            <h1>Athlete Palpation Screen</h1>
+        );
+    }
+}
+
+export default AthletePalpation;
